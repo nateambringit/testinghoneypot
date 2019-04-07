f77a08bb0ee322964a87c93427df5aef5db7f890
Naufal Arkaan <naufalarkaan.arkaan@gmail.com>
2019-04-07 18:57:35 +0700

    Create configmail.py

---
+++ configmail.py
@@ -0,0 +1,4 @@
+mailToAdress = "naufalarkaan.arkaan@gmail.com"
+mailFromServer = "smtp.gmail.com:587"   #e.g. smtp.gmail.com:587 for gmail
+mailFromAdress = "pemberitahuanhoneypot@gmail.com"
+mailFromPassword = "tugasakhirserver"
