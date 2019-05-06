ddc883f43a72de6156c9be59b96414f5fb3c1026
Naufal Arkaan <naufalarkaan.arkaan@gmail.com>
2019-05-06 13:46:35 +0700

    Update configmail.py

--- configmail.py
+++ configmail.py
@@ -1,4 +1,4 @@
-mailToAdress = "naufalarkaan.arkaan@gmail.com"
+mailToAdress = "yourmailtoaddress@email.com"
 mailFromServer = "smtp.gmail.com:587"   #e.g. smtp.gmail.com:587 for gmail
-mailFromAdress = "pemberitahuanhoneypot@gmail.com"
-mailFromPassword = "tugasakhirserver"
+mailFromAdress = "youremailfromaddress@email.com"
+mailFromPassword = "yourpassword"
